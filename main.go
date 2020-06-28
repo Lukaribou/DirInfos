6df3462306ea8d716de36c50717f6b828ccd2c73
Lukaribou <lucasperroud17@gmail.com>
2020-06-28 17:41:19 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"os/exec"
 )
 
 func main() {
+	infos := GetInfos(getCmdUserPosition())
 
+	fmt.Printf(
+		`
+Infos sur le dossier: "%s"
+
+Disque: %s
+Sous dossiers: %d (Accès refusés: %d)
+Fichiers: %d
+Taille: %do
+`,
+		infos.FolderName, infos.DiskName, infos.TotalSubFolders, infos.TotalSubFiles, infos.DirsAccessDenied, infos.TotalSize)
 }
 
-func getCmdUserPosition() (string, error) {
+func getCmdUserPosition() string {
 	out, err := exec.Command("cmd", "/C", "echo", "%CD%").CombinedOutput()
-	if err != nil {
-		return "", err
-	}
-	return string(out), nil
+	CheckAndPanic(err)
+	return RemoveNonVisibleChars(string(out))
 }
