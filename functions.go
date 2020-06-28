834f94f35cb8114cf93d39992e4d1517384c6f50
Lukaribou <lucasperroud17@gmail.com>
2020-06-28 20:10:58 +0200

    Update functions.go

--- functions.go
+++ functions.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"bytes"
+	"strconv"
+)
+
 // CheckAndPanic : Panic si err != nil
 func CheckAndPanic(err error) {
 	if err != nil {
@@ -16,4 +21,31 @@ func RemoveNonVisibleChars(str string) string {
 		}
 	}
 	return string(temp)
+}
+
+// SpaceEvery3 : Renvoie le nombre avec un espace tous les 3 chiffres
+func SpaceEvery3(n uint) string {
+	return reverse(insertNth(reverse(strconv.FormatUint(uint64(n), 10)), ' ', 3))
+}
+
+func reverse(str string) string { // https://stackoverflow.com/questions/1752414/how-to-reverse-a-string-in-go
+	runes := []rune(str)
+
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func insertNth(str string, toInsert rune, allTheN int) string { // https://stackoverflow.com/questions/33633168/how-to-insert-a-character-every-x-characters-in-a-string-in-golang
+	var buffer bytes.Buffer
+
+	for i, rune := range str {
+		buffer.WriteRune(rune)
+		if i%allTheN == allTheN-1 && i != len(str)-1 {
+			buffer.WriteRune(toInsert)
+		}
+	}
+
+	return buffer.String()
 }
